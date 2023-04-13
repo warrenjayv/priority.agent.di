9f53923755c8aead8466e16c5e5458624ee157ae
battle_angel <six@angel>
2023-04-13 04:47:00 -0400

    dial.ops.py

--- dial.ops.py
+++ dial.ops.py
@@ -1,4 +1,34 @@
 ### dial ops
+
+Wed Apr 12 22:59:18 EDT 2023
+# priority.agent.html
+six: on station. need a socket to update & retrieve file
+2326
+hanna: we need httpd installed locally. 
+angel: installing httpd via cygwin
+2358
+six: httpd installed locally for testing.
+0210
+angel: the php worked in ec2. alot of php packages are required. 
+0422
+six: the chatbox is now successful!
+becky: we need the following:
+   + check file size to ensure memory is not spammed.
+   + load the chat onload
+0446
+six: chatbox is ready but will need a file checker
+
+Tue Apr 11 20:11:22 EDT 2023
+# priority.agent.thml 
+six: the linux & httptd server is uP!
+hanna: telnet connection success
+21:32~
+angel: learning javascript
+hanna: nav pannel added
+23:34~
+six: linking javascript source
+ 
+
 Sun Apr  9 19:50:26 EDT 2023
 six: task is to test telnet against a socket bound to the appropriate 
      telnet port. 
